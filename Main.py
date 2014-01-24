8f60fdb2be7de8710b252f5cebfadd1c0d4f7814
ScepticRail <paul.fady@gmail.com>
2014-01-24 19:55:54 +0000

    Completed temporary version
    This is the completed version that I will use for now. It is likely that there is a better way to do almost all of this.

--- Main.py
+++ Main.py
@@ -4,8 +4,9 @@ def ask(question):
 
 fail = "Please answer with either Y or N.\n If you did answer with Y or N, it is likely that I do not know what your bacterium is."
 
-print "This is BacteriaKey v0.15"
+print "This is BacteriaKey v0.3"
 print "This was created as a side project by ScepticRail when attempting to learn Python."
+print "This dichotomous key is largely based on that of the University of Wyoming."
 print "Any and all errors are, of course, intentional. Reporting them is futile."
 
 print""
@@ -19,12 +20,12 @@ grampos = ask("Is your bacterium Gram-positive?")
 if grampos:
 	
 	# Staphylococcus and Micrococcus are catalase positive. Streptococcus and Enterococcus. are catalase negative.
-	catalase = ask("Is your bacterium catalase positive?") 
+	catalase = ask("Is your bacterium catalase-positive?") 
 	
 	if catalase:		
 		
 		# This test differentiates Staphylococcus aureus from other coagulase negative Staphylococcus species.
-		coagulase = ask("Is your bacterium coagulase positive?") 
+		coagulase = ask("Is your bacterium coagulase-positive?") 
 		
 		if coagulase:
 			print "Your bacterium is STAPHYLOCOCCUS AUREUS."
@@ -47,7 +48,7 @@ if grampos:
 			
 			if esculin:
 			
-				optochin = ask("Is your bacterium resistant to optochin?")
+				optochin = ask("Is your bacterium optochin-resistant?")
 				
 				if optochin:
 					print "Your bacterium is STREPTOCOCCUS MITIS."
@@ -91,24 +92,130 @@ if grampos:
 elif not grampos:
 	
 	# used to distinguish between oxidase negative Enterobacteriaceae and oxidase positive Pseudomadaceae. 
-	oxidase = ask("Is your bacterium oxidase positive?") 
+	oxidase = ask("Is your bacterium oxidase-positive?") 
 	
 	if oxidase:
 	
-		nitrate = ask("Is your bacterium capable of reducing nitrate to nitrate or othe nitrogenous compounds?")
+		nitrate = ask("Is your bacterium nitratase-positive?")
 			
 		if nitrate:
-			print "Your bacterium is STAPHYLOCOCCUS EPIDERMIDIS."
+			print "Your bacterium is PSEUDOMONAS AERUGINOSA."
 					
 		elif not nitrate:
-			print: "
+			print "Your bacterium is ALCALIGENES FAECALIS."
 			
 		else:	
 			print fail
 		
 	elif not oxidase:
-		print PLACEHOLDER
+		
+		# crazy complicated
+		lactose = ask("Is your bacterium capable of fermenting lactose?")
+		
+		if lactose:
+		
+			#tests an organism's ability to ferment glucose 
+			durham = ask("Is your bacterium hydrogen lyase-positive?")
+			
+			if durham:
+				
+				# can your bacterium swim through a medium called SIM (who' knows)
+				motile = ask("Is your bacterium motile?")
+				
+				if motile:
+				
+					mixed_acid = ask("Does your bacterium utilise the mixed acid fermentation pathway (MR+)?")
+					
+					if mixed_acid:
+						print "Your bacterium is CITROBACTER FREUNDII."
+						
+					elif not mixed_acid:
+						print "Your bacterium is ENTEROBACTER AEROGENES."
+					
+					else:
+						print fail
+						
+				elif not motile:
+				
+					# Klebsiella pneumoniae and Proteus mirabilis are citrate positive. Escherichia coli and Shigella dysenteriae are citrate negative.
+					citrate = ask("Is your bacterium capable of using citrate as its sole carbon source?")
+					
+					if citrate:
+						
+						# Also complicated, full of stupid reagents
+						tryptophan = ask("Is your bacterium able to convert tryptophan to indole?")
+						
+						if tryptophan:
+							print "Your bacterium is KLEBSIELLA OXYTOCA."
+						
+						elif not tryptophan:
+							
+							nitrate = ask("Is your bacterium capable of reducing nitrate to nitrate or othe nitrogenous compounds?")
+			
+							if nitrate:
+								print "Your bacterium is PSEUDOMONAS AERUGINOSA."
+					
+							elif not nitrate:
+								print "Your bacterium is ACTINOBACTER CALCOACETICUS."
+			
+							else:	
+								print fail
+							
+						else:
+							print fail
+						
+					elif not citrate:
+						print "Your bacterium is ESCHERICHIA COLI."
+						
+					else:
+						print fail
+				
+				else:
+					print fail
+					
+			elif not durham:
+				print "Your bacterium is SERRATIA MARCESCENS."
+				
+			else:
+				print fail
 	
+		elif not lactose:
+		
+			# can your bacterium swim through a medium called SIM (who' knows)
+			motile = ask("Is your bacterium motile?")
+			
+			if motile:
+					
+					tryptophan = ask("Is your bacterium able to convert tryptophan to indole?")
+						
+					if tryptophan:
+						print "Your bacterium is KLEBSIELLA OXYTOCA."
+						
+					elif not tryptophan:
+						
+						urease = ask("Is your bacterium urease-positive?")
+							
+						if urease:
+							print "Your bacterium is PROTEUS MIRABILIS."
+								
+						elif not urease:
+							print "Your bacterium is SALMONELLA TYPHIMURIUM."
+								
+						else:
+							print fail
+						
+					else:
+						print fail
+			
+			elif not motile:
+					print "Your bacterium is SHIGELLA DYSENTERIAE."
+				
+			else:
+					print fail
+		
+		else:
+			print fail
+			
 	else:
 		print fail
 
